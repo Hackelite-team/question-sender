edcacf6ce00831135eb3b8e65eb2b4662f7767ad
Nivedha19 <sec19cs030@sairamtap.edu.in>
2020-09-04 16:36:01 +0530

    inserted frame1 and frame2

--- qpsend.py
+++ qpsend.py
@@ -5,6 +5,10 @@ window=Tk()
 window.title("Question paper Setter")
 window.geometry("850x600+0+0")
 window.config(bg="#9B59B6")
+frame1=LabelFrame(window,text="number of students",padx=60,pady=10,bg="#9B59B6")
+frame1.grid(row=0,column=0,padx=5,pady=10)
 
+frame2=LabelFrame(window,text="Mail id of students",padx=5,pady=10,bg="#9B59B6")
+frame2.grid(row=0,column=1,padx=5,pady=10)
 
 window.mainloop()
